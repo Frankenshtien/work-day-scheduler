71951dd595f3f28b6e67ad39e55adcab2a84087a
Kyle Z Condie <barber.von.frankenshtien@gmail.com>
2020-12-05 21:41:40 -0700

    got some stuff going

---
+++ assets/javascript/script.js
@@ -0,0 +1,27 @@
+
+
+$(document).ready(function() {
+    $("#currentDay").text(moment().format("MMM Do YYYY, h:mm a"));
+});
+
+setInterval(function() {
+    $("#currentDay").text(moment().format("MMM Do YYYY, h:mm a"));
+  }, 1000);
+
+$(".description").on("click", function() {
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("col-10 description past").val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".saveBtn").on("click", function() {
+    var text = $(this).parent().children(".description").val().trim();
+    var descriptionDiv = $("<div>").addClass("col-10 description past").text(text);
+    var rowTime = $(this).parent().attr("id");
+    console.log(rowTime);
+    $(this).parent().children(".description").replaceWith(descriptionDiv);
+
+    localStorage.setItem(rowTime, JSON.stringify($(this).parent().children(".description")))
+});
+
